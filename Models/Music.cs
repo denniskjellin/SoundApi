bffef69cae8b041614198dca92be6fb8dec3a890
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-20 12:43:17 +0100

    Added DataAnnotation schema and more specific added foreignKey to the file

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,23 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SoundApi.Models {
 
     public class Music {
         // Properties
-        public int Id { get; set; }
+        public int MusicId { get; set; }
         [Required]
         public string? Artist { get; set; }
         [Required]
         public string? Title { get; set; }
         [Required]
         public int? Length { get; set; }
+
+        [ForeignKey("MusicGenre")]
+        public int GenreId { get; set; }
         public Genre? MusicGenre { get; set; }
 
     }
 
     public class Genre {
         // Properties
-        public int Id { get; set; }
+        public int GenreId { get; set; }
         [Required]
         public string? Name { get; set; }
     }
