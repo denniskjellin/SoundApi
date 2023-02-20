ddae7e16231ba709c2a9db5f9dff18a724b1372f
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-20 11:51:51 +0100

    Creating dbContext file

---
+++ Models/Data/MusicContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SoundApi.Models;
+
+namespace SoundApi.Data {
+    public class MusicContext : DbContext {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options) { 
+
+        }
+        // Table names and property
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
