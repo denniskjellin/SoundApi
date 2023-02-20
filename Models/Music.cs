0084d6df3e2e54a1326069e095be1eb7a432b9b1
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-20 11:44:28 +0100

    Added [Required] fields and included DataAnnotations

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SoundApi.Models {
 
     public class Music {
         // Properties
         public int Id { get; set; }
+        [Required]
         public string? Artist { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public int? Length { get; set; }
-
         public Genre? MusicGenre { get; set; }
 
     }
@@ -14,6 +18,7 @@ namespace SoundApi.Models {
     public class Genre {
         // Properties
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
     }
 }
