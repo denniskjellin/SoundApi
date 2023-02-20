de51deb38d6456b3af9fc9177394a4d1c7483387
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-20 14:26:36 +0100

    trying a different aproach

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SoundApi.Models {
+namespace SoundApi.Models
+{
 
-    public class Music {
+    public class Music
+    {
         // Properties
         public int MusicId { get; set; }
         [Required]
@@ -12,17 +14,20 @@ namespace SoundApi.Models {
         public string? Title { get; set; }
         [Required]
         public int? Length { get; set; }
-        [Required]
-        [ForeignKey("GenreId")]
-        public int GenreId { get; set; }
-        public Genre? MusicGenre { get; set; }
 
+        // Setting a foreign key property (connecting Music and genre)
+        public int GenreId { get; set; }
+
+        // Setting a navigation property (get the genre)
+        [ForeignKey("GenreId")]
+        public Genre? Genre { get; set; }
     }
 
-    public class Genre {
+    public class Genre
+    {
         // Properties
         public int GenreId { get; set; }
         [Required]
-        public string? Name { get; set; }
+        public string? GenreName { get; set; }
     }
 }
